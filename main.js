a8f5e1b0f4b0cf52b39a15ac2b2994ffaeb63af6
ZPPD <zhana.p.d@gmail.com>
2018-10-23 11:53:49 -0400

    random color logic

--- main.js
+++ main.js
@@ -10,10 +10,39 @@ var colors = [
 var squares = document.querySelectorAll('.square');
 var colorDisplay = document.getElementById('colorDisplay');
 
-var pickedColor = colors[3];
+var pickedColor = pickColor();
+var messageDisplay = document.querySelector('#message');
 
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
+    //add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
+
+    //add click listeners to squares
+    squares[i].addEventListener('click', function(){
+        //grab color of clicked square
+        var clickedColor = this.style.backgroundColor;
+        //compare color to pickedColor 
+        if(clickedColor === pickedColor){
+            messageDisplay.textContent = 'Correct!';
+            changeColors(clickedColor);
+        } else {
+            this.style.backgroundColor = '#232323';
+            messageDisplay.textContent = 'Try Again';
+        }
+    });
+}
+
+function changeColors(color){
+    //loop through all the squares
+    for(var i = 0; i < squares.length; i++){
+    //change each color to match given color
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor(){
+   var random = Math.floor(Math.random() * colors.length);
+   return colors[random];
 }
