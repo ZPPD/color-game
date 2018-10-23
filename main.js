1abb64111f50be30c4067999bc4bfdd6a65e156f
ZPPD <zhana.p.d@gmail.com>
2018-10-23 11:40:44 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var colors = [
+    'rgb(255, 0, 0)',
+    'rgb(255, 255, 0)',
+    'rgb(0, 255, 0)',
+    'rgb(0, 255, 255)',
+    'rgb(0, 0, 255)',
+    'rgb(255, 0, 255)',
+];
+
+var squares = document.querySelectorAll('.square');
+var colorDisplay = document.getElementById('colorDisplay');
+
+var pickedColor = colors[3];
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+}
